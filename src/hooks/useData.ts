ec72dba04a66148b62ede0a523cb4aecdf87e2ee
kykim00 <jeongsaeki@likelion.org>
2021-12-25 20:12:31 +0900

    ♻️ Refactor custom Hooks

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,9 +1,6 @@
-import axios from "axios";
 import useSWR from "swr";
 import { FUTURAMA_API_ENDPOINT } from "../constants";
-
-const fetcher = (url: string) => axios(url).then((res) => res.data);
-
+import { fetcher } from "../utils/fetcher";
 export const useData = (pageName: string) => {
   return useSWR(`${FUTURAMA_API_ENDPOINT}${pageName}`, fetcher);
 };
