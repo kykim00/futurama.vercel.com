30a1652bfe32df65a8fab9638b173e8f00acb7ae
kykim00 <jeongsaeki@likelion.org>
2021-12-27 17:13:21 +0900

    ✨ : 상세페이지 구현

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,42 @@
 export const FUTURAMA_API_ENDPOINT = "https://api.sampleapis.com/futurama/";
 export const MAIN_IMAGE_URL =
   "https://images7.alphacoders.com/596/thumb-1920-596242.jpg";
+export const IMAGE_URL = {};
+export const ROUTES = [
+  {
+    ID: 0,
+    PATH: "/info",
+    LABEL: "Info",
+    ORDER: 0,
+  },
+  {
+    ID: 1,
+    PATH: "/characters",
+    LABEL: "Characters",
+    ORDER: 1,
+  },
+  {
+    ID: 2,
+    PATH: "/cast",
+    LABEL: "Cast",
+    ORDER: 2,
+  },
+  {
+    ID: 3,
+    PATH: "/episodes",
+    LABEL: "Episodes",
+    ORDER: 3,
+  },
+  {
+    ID: 4,
+    PATH: "/questions",
+    LABEL: "Questions",
+    ORDER: 4,
+  },
+  {
+    ID: 5,
+    PATH: "/inventory",
+    LABEL: "Inventory",
+    ORDER: 5,
+  },
+];
