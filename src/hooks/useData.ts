3ebef65dd59534616fb599855844f9b4b1c5d9a5
kykim00 <jeongsaeki@likelion.org>
2021-12-27 17:10:44 +0900

    🐛 : useSWR 두번 호출 오류 fix

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,6 +1,10 @@
 import useSWR from "swr";
 import { FUTURAMA_API_ENDPOINT } from "../constants";
 import { fetcher } from "../utils/fetcher";
-export const useData = (pageName: string) => {
+export const useData = (
+  pageName: string,
+  path: string | string[] | undefined
+) => {
+  if (path) pageName += "/" + path;
   return useSWR(`${FUTURAMA_API_ENDPOINT}${pageName}`, fetcher);
 };
